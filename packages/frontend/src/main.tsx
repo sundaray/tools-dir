d5e78008f5d18a48610b143f7c3440969ebb9d32
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-07-22 12:19:50 +0530

    created src folder

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -13,6 +13,7 @@ declare module "@tanstack/react-router" {
 }
 
 const rootElement = document.getElementById("root");
+
 if (!rootElement?.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
