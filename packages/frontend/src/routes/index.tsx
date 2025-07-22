989521696340abed5334a9356db656e3c231c29a
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-07-22 14:24:43 +0530

    shadcn working as expected

--- packages/frontend/src/routes/index.tsx
+++ packages/frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -10,6 +11,7 @@ export default function Home() {
     <div className="p-2">
       <h2 className="text-2xl text-neutral-900">Hello World</h2>
       <Input className="border-neutral-300" />
+      <Button>Hello</Button>
     </div>
   );
 }
