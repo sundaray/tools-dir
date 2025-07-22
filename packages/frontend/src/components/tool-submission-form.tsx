ce43c8ec45e0587e98f59b88f986ba4366a1a676
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-07-22 16:38:57 +0530

    installed axios

--- packages/frontend/src/components/tool-submission-form.tsx
+++ packages/frontend/src/components/tool-submission-form.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { effectTsResolver } from "@hookform/resolvers/effect-ts";
 import { ToolSubmissionSchema, type ToolSubmissionFormData } from "@/schema";
